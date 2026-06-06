12b7f7ce26aa672e353ada04c53814c5917f124b
tianhai2026 <tianhai2026@github.com>
2026-06-07 06:14:22 +0800

    Remove static export for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
   headers: async () => [
     {
       source: '/sw.js',
